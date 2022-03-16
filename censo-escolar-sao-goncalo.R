219ac11a0e3587d83f65c351760872ff26926318
Carolina Carvalho <62574038+carvaio@users.noreply.github.com>
2022-03-16 11:45:57 -0300

    Add files via upload

---
+++ censo-escolar-sao-goncalo.R
@@ -0,0 +1,11 @@
+library("basedosdados")
+library(dplyr)
+
+set_billing_id("censo-escolar-340817")
+
+base = read_sql("SELECT * FROM `basedosdados.br_inep_censo_escolar.turma` WHERE id_municipio= \'3304904\'",page_size=9000)
+
+base = base %>% filter(ano==2020)
+base = base %>% filter(rede=='municipal')
+
+write.csv(base,"sao-goncalo.csv")
