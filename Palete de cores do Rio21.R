7cce7d58e9b3ebcdb0d73444911a7d936402643e
Carolina Carvalho <62574038+carvaio@users.noreply.github.com>
2022-03-16 14:33:00 -0300

    Create Palete de cores do Rio21.R

---
+++ Palete de cores do Rio21.R
@@ -0,0 +1,10 @@
+library(paletti)
+
+#cores em HEX
+institutorio21= c("roxo"="#5D4898",
+                  "azul_es"="#4385C5",
+                  "azul_cl"="#26AFC8",
+                  "verde"="#5DB131",
+                  "amarelo"="#FAC026")
+
+viz_palette(institutorio21)
